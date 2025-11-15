2cd3c6b70fae20e650cd98dc1f1cf508e23918c1
Ashmoon32 <nyeinmaung3021@gmail.com>
2025-11-15 14:52:17 +0630

    .

--- Sample_Project/project/profile.php
+++ Sample_Project/project/profile.php
@@ -16,6 +16,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
 </head>
 <body>
     <div class="container mt-5">
