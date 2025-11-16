ff4ef40f5b753e5163ecf146056d2e96f39de201
Ashmoon32 <nyeinmaung3021@gmail.com>
2025-11-16 19:22:10 +0630

    -

--- form/index.php
+++ form/index.php
@@ -7,9 +7,9 @@
 </head>
 <body>
     <h1>PHP Request</h1>
-    <form action="request.php" method="get">
-        <input type="text" name="name" placeholder="Name"><br>
-        <input type="text" name="age" placeholder="Age"><br>
+    <form action="request.php" method="post">
+        <input type="text" name="name" placeholder="Name"><br><br>
+        <input type="text" name="age" placeholder="Age"><br><br>
 
         <button type="submit">Send Data</button>
     </form>
