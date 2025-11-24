b4158afc662a10afdc8a05ed7e47ef28d11a3b7d
Ashmoon32 <nyeinmaung3021@gmail.com>
2025-11-24 13:00:35 +0000

    Practice

--- OOP/test2.php
+++ OOP/test2.php
@@ -18,3 +18,38 @@ class Animal
 $dog = new Animal("Bobby");
 var_dump($dog);
 $dog->run();
+
+
+echo "\n------------------------------------\n";
+
+
+class Dog
+{
+    private $dog_name = "Booby";
+
+    public function bark()
+    {
+        echo "$this->dog_name is barking...";
+    }
+}
+
+$DOG = new Dog;
+$DOG->bark();
+
+class Animal1 
+{
+    public function __construct(private $name)
+    {
+        //
+    }
+
+    public function run()
+    {
+        echo "$this->name is running...";
+    }
+}
+
+$dog1 = new Animal1("Aung Net");
+echo "\n";
+$dog1->run();
+echo "\n";
