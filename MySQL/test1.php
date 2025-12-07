facbca5bac6c621d5767406f086ae0db6916c71a
Ashmoon32 <nyeinmaung3021@gmail.com>
2025-12-07 22:24:11 +0630

    Practice

--- MySQL/test1.php
+++ MySQL/test1.php
@@ -6,7 +6,25 @@ $db = new PDO('mysql:dbhost=localhost;dbname=project', 'root', '', [
 ]);
 
 $statement = $db->query('SELECT * FROM roles');
+$sql = "INSERT INTO roles(name, value) VALUES ('Supervisor', 4)";
+$db->query($sql);
 
-$result = $statement->fetchAll();
 
-print_r($result);
+$row1 = $statement->fetch();
+$row2 = $statement->fetch();
+
+print_r($row1);
+print_r($row2);
+echo "\n";
+echo $db->lastInsertId();
+
+$sql1 = "INSERT INTO roles ( name, value ) VALUES ( :name, :value )";
+
+$statement2 = $db->prepare($sql1);
+
+$statement2->execute([
+    'name' => "God",
+    'value' => 999
+]);
+
+echo $db->lastInsertId();
