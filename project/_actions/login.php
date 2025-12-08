cd5d56ddd129c2ad1454684fdf2eaf76ef6f9413
Ashmoon32 <nyeinmaung3021@gmail.com>
2025-12-08 17:37:37 +0630

    Practice

--- project/_actions/login.php
+++ project/_actions/login.php
@@ -5,13 +5,9 @@ session_start();
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-
-if ($email === 'ashmoon32@gmail.com' and $password === "ashmoon32") {
+if ($email === 'ashmoon32@gmail.com' and $password === 'ashmoon32') {
     $_SESSION['user'] = ['username' => 'Ashmoon'];
     header('location: ../profile.php');
-} elseif ($email === 'nyeinmaung32@gmail.com' and $password === 'nyeinmaung32') {
-    $_SESSION['user'] = ['username' => 'Nyein Maung'];
-    header('location: ../profile2.php');
 } else {
     header('location: ../index.php?incorrect=1');
 }
--- project/_actions/upload.php
+++ project/_actions/upload.php
@@ -1,29 +1,18 @@
-<?php 
-session_start();
+<?php
+
 $error = $_FILES['photo']['error'];
 $tmp = $_FILES['photo']['tmp_name'];
 $type = $_FILES['photo']['type'];
 
-if ($error)
+if($error)
 {
-    header('location: ../profile2.php?error=file');
+    header('location: ../profile.php?error=file');
     exit();
 }
 
-if ($type === "image/jpeg" or $type === "image/png") {
-    // move_uploaded_file($tmp, "photos/profile.jpg");
-    // header('location: ../profile.php');
-    $username = $_SESSION['user']['username'];
-
-    $filePath = "photos/" . $username . ".jpg";
-
-    move_uploaded_file($tmp, $filePath);
-
-    if ($username === "Ashmoon") {
-        header('location: ../profile.php');
-    } else {
-        header('location: ../profile2.php');
-    }
+if($type === "image/jpeg" or $type === "image/png") {
+    move_uploaded_file($tmp, "photos/profile.jpg");
+    header('location: ../profile.php');
 } else {
     header('location: ../profile.php?error=type');
 }
