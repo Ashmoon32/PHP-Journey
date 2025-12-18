65e9e514a0db455b1c542a848e5aaaed71d9be8a
Ashmoon32 <nyeinmaung3021@gmail.com>
2025-12-18 23:12:11 +0630

    Practice Controller

--- Laravel-Project/Blog/routes/web.php
+++ Laravel-Project/Blog/routes/web.php
@@ -1,23 +1,36 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\Product\ProductController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/articles', function() {
-    return 'Article List';
-});
+// Route::get('/articles', function() {
+//     return 'Article List';
+// });
 
-Route::get('/articles/details', function() {
-    return 'Article Detail';
-})->name('article.detail');
+// Route::get('/articles/details', function() {
+//     return 'Article Detail';
+// })->name('article.detail');
 
-Route::get('/articles/details/{id}', function($id) {
-    return "Article Detail - $id";
-});
+// Route::get('/articles/details/{id}', function($id) {
+//     return "Article Detail - $id";
+// });
 
-Route::get('/articles/more', function() {
-    return redirect()->route('article.detail');
-});
+// Route::get('/articles/more', function() {
+//     return redirect()->route('article.detail');
+// });
+
+Route::get('/', [ArticleController::class, 'index']);
+
+Route::get('/articles', [ArticleController::class, 'index']);
+
+Route::get('/articles/detail/{id}', [
+    ArticleController::class,
+    'detail'
+]);
+
+Route::get('/products', [ProductController::class, 'index']);
