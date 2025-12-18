4433a069cdb03c0353fec3d24fa6655749df1fe7
Ashmoon32 <nyeinmaung3021@gmail.com>
2025-12-18 21:28:20 +0630

    Practice Routing

--- Laravel-Project/Blog/routes/web.php
+++ Laravel-Project/Blog/routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/articles', function() {
+    return 'Article List';
+});
+
+Route::get('/articles/details', function() {
+    return 'Article Detail';
+})->name('article.detail');
+
+Route::get('/articles/details/{id}', function($id) {
+    return "Article Detail - $id";
+});
+
+Route::get('/articles/more', function() {
+    return redirect()->route('article.detail');
+});
