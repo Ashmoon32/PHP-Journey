432b897d2994fd62825d421f1b02067318276ed4
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-01-18 13:31:43 +0630

    add main page

---
+++ anime-character-search/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Anime in a Nutshell | Ashmoon</title>
+</head>
+
+<body>
+    <h3>Search your favorite anime character.</h3>
+    <form action="card.php" method="GET">
+        <input type="text" name="character" placeholder="Enter character name" required>
+        <button type="submit">Search</button>
+    </form>
+</body>
+
+</html>
