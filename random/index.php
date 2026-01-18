1fbf81245c6c45c3a9680094a312619033791854
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-01-18 10:26:34 +0630

    -

---
+++ random/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Anime in a Nutshell | Ashmoon</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Welcome to the Dummy Nutshell!</h1>
+        <h3>Search Your Favorite Anime Characters Below...</h3>
+        <form action="controller.php" method="GET">
+            <label for="characterName">Enter Character Name</label> <br>
+            <input type="text" name="characterName" id="characterName" required> <br><br>
+
+            <input type="submit" value="Search" name="search">
+        </form>
+    </div>
+
+</body>
+
+</html>
