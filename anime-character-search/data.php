f808c1026eb003b452dbc662eee5b892a747a643
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-01-18 15:03:06 +0630

    add images

--- anime-character-search/data.php
+++ anime-character-search/data.php
@@ -5,34 +5,39 @@ $animeCharacters = [
         "name" => "Light Yagami",
         "anime" => "Death Note",
         "power" => "Death Note",
-        "personality" => "Intelligent, Manipulative"
+        "personality" => "Intelligent, Manipulative",
+        "image" => "./images/light-yagami.jpg",
     ],
 
     "naruto uzumaki" => [
         "name" => "Naruto Uzumaki",
         "anime" => "Naruto",
         "power" => "Nine-Tails Chakra",
-        "personality" => "Energetic, Never Gives Up"
+        "personality" => "Energetic, Never Gives Up",
+        "image" => "./images/naruto-uzumaki.jpg",
     ],
 
     "luffy" => [
         "name" => "Monkey D. Luffy",
         "anime" => "One Piece",
         "power" => "Gum-Gum Fruit",
-        "personality" => "Carefree, Brave"
+        "personality" => "Carefree, Brave",
+        "image" => "./images/luffy.jpg",
     ],
 
     "eren yeager" => [
         "name" => "Eren Yeager",
         "anime" => "Attack on Titan",
         "power" => "Titan Shifting",
-        "personality" => "Determined, Emotional"
+        "personality" => "Determined, Emotional",
+        "image" => "./images/eren-yeager.jpg"
     ],
 
     "goku" => [
         "name" => "Goku",
         "anime" => "Dragon Ball",
         "power" => "Ultra Instinct",
-        "personality" => "Pure-hearted, Fighter"
+        "personality" => "Pure-hearted, Fighter",
+        "image" => "./images/goku.jpg"
     ]
 ];
