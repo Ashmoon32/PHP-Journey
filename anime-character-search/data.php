22e77e8a7170fd4db8c93262f0ea8d6898b25071
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-01-18 13:32:57 +0630

    add manual data

---
+++ anime-character-search/data.php
@@ -0,0 +1,38 @@
+<?php
+
+$animeCharacters = [
+    "light yagami" => [
+        "name" => "Light Yagami",
+        "anime" => "Death Note",
+        "power" => "Death Note",
+        "personality" => "Intelligent, Manipulative"
+    ],
+
+    "naruto uzumaki" => [
+        "name" => "Naruto Uzumaki",
+        "anime" => "Naruto",
+        "power" => "Nine-Tails Chakra",
+        "personality" => "Energetic, Never Gives Up"
+    ],
+
+    "luffy" => [
+        "name" => "Monkey D. Luffy",
+        "anime" => "One Piece",
+        "power" => "Gum-Gum Fruit",
+        "personality" => "Carefree, Brave"
+    ],
+
+    "eren yeager" => [
+        "name" => "Eren Yeager",
+        "anime" => "Attack on Titan",
+        "power" => "Titan Shifting",
+        "personality" => "Determined, Emotional"
+    ],
+
+    "goku" => [
+        "name" => "Goku",
+        "anime" => "Dragon Ball",
+        "power" => "Ultra Instinct",
+        "personality" => "Pure-hearted, Fighter"
+    ]
+];
