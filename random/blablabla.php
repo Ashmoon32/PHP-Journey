e6de3c3da65ab9d9e55d6f18b3a99f1efbf2a759
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-01-21 22:43:31 +0630

    -

--- random/blablabla.php
+++ random/blablabla.php
@@ -3,4 +3,11 @@
 $nothing = fn($none = "Nothing.") => $none;
 echo $nothing("do nothing");
 
-
+// spaceship operator
+$a = 5;
+$b = 10;
+$c = 12;
+echo $a <=> $b;
+echo $b <=> $a;
+echo $b <=> $c;
+echo $b <=> $b;
