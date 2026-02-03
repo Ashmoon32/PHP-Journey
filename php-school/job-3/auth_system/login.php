540c69989cdc050d46ab8d9916a92895db40dd3a
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-02-03 23:26:22 +0630

    login page

---
+++ php-school/job-3/auth_system/login.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+
+$valid_username = "Ashmoon";
+$valid_password = "ashmoon322005";
+$err = "";
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $user = $_POST['username'];
+    $pass = $_POST['password'];
+
+    if ($user === $valid_username && $pass === $valid_password) {
+        $_SESSION['username'] = $user;
+        header("location: dashboard.php");
+        exit();
+    } else {
+        $err = "Incorrect username or password! Please try again.";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Login Page | Ashmoon</title>
+</head>
+
+<body>
+    <h1>Login Form | Ashmoon</h1>
+    <form action="" method="POST">
+        <label for="username">Username</label><br>
+        <input type="text" name="username" id="username" placeholder="Enter your name" required><br><br>
+
+        <label for="password">Password</label><br>
+        <input type="password" name="password" id="password" placeholder="Enter your password" required><br><br>
+
+        <button type="submit">Login</button>
+    </form>
+</body>
+
+</html>
