47640379200261255d291777b42a63a5c644bf1f
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-02-03 23:50:54 +0630

    add dashboard page :if authenticate , go to the dashboard

---
+++ php-school/job-3/auth_system/dashboard.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header("location: login.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Dashboard Page | Ashmoon</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Welcome to the Dashboard,
+            <?= $_SESSION['username']; ?>
+        </h1>
+        <p>Now you are authenticated.</p>
+        <p>What is your agenda today?</p>
+        <textarea name="messages" cols="23" rows="3"
+            style="resize:none; border: 2px solid black; border-radius: 10px; padding: 10px"></textarea>
+    </div>
+</body>
+
+</html>
