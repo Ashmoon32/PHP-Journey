9fac942f1b2e62f618ce6f8798175b9ce198cfe4
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-02-07 23:51:29 +0630

    -

--- authentication/register.php
+++ authentication/register.php
@@ -11,23 +11,37 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration Page</title>
+    <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet">
+
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: "VT323", monospace;
+        }
+    </style>
 </head>
 
 <body>
-    <h1>Welcome to the Ashmoon World, <?= $username; ?></h1>
-    <p>Regsiter below to enter Ashmoon World.</p>
-    <form action="" method="POST">
-        <label for="name">Username</label>
-        <input type="text" name="username" id="username" placeholder="Enter your name" required>
+    <div class="container">
+        <h1>Welcome to the Ashmoon World, <?= $username; ?>
+        </h1>
+        <p>Regsiter below to enter Ashmoon World.</p>
+        <form method="POST">
+            <label for="name">Username</label>
+            <input type="text" name="username" id="username" placeholder="Enter your name" required>
 
-        <label for="email">Email</label>
-        <input type="email" name="email" id="email" placeholder="Enter your email" required>
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" placeholder="Enter your email" required>
 
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password" placeholder="Enter your password" required>
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" placeholder="Enter your password" required>
 
-        <button type="submit">Sign in</button>
-    </form>
+            <button type="submit">Sign in</button>
+        </form>
+        <small>Already have an account? <a href="login.php">Sign up</a></small>
+    </div>
 </body>
 
 </html>
