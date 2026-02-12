03feaa7575a6790fffcb78c4e70211b812bc4135
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-02-12 13:17:43 +0630

    -

---
+++ php-school/ch-10/exe2.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Products Info</h1>
+    <?php
+    try {
+        $pdo = new PDO("mysql:host=localhost;dbname=my_database", "root", "");
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $stmt = $pdo->query("SELECT * FROM products");
+        echo "<table border='1'>";
+        echo "<tr><th>ID</th><th>Name</th><th>Price</th><th>Category</th></tr>";
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            echo "<tr>";
+            echo "<td>{$row['id']}</td>";
+            echo "<td>{$row['name']}</td>";
+            echo "<td>{$row['price']}</td>";
+            echo "<td>{$row['category']}</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+    } catch (PDOException $e) {
+        echo "Error" . $e->getMessage();
+    }
+    ?>
+</body>
+
+</html>
