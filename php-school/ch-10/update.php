ec2da5a920b518b64bd7e01ecaaa4cdfc58d675c
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-02-15 20:19:05 +0630

    -

---
+++ php-school/ch-10/update.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Update Page</title>
+</head>
+
+<body>
+    <form action="" method="POST">
+        Prodcut ID <input type="number" name="name" required><br>
+        New Price <input type="number" name="price" required><br>
+        New Category <input type="text" name="category" required><br>
+        <input type="submit" name="submit" value="Update Product">
+    </form>
+    <?php
+    if (isset($_POST['submit'])) {
+        $pdo = new PDO("mysql:host=localhost;dbname=my_database", "root", "");
+        $stmt = $pdo->prepare("UPDATE products SET price = :price, category = :category WHERE id = :id");
+        $stmt->execute([
+            ':id' => $_POST['id'],
+            ':price' => $_POST['price'],
+            ':category' => $_POST['category']
+        ]);
+        echo "Updated Product!";
+
+    }
+    ?>
+</body>
+
+</html>
