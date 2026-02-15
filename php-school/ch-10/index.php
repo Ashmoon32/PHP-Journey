ffa4c063ac0f4cb4b58e5ffee0cf16671f6e6f2e
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-02-15 19:12:32 +0630

    -

--- php-school/ch-10/index.php
+++ php-school/ch-10/index.php
@@ -8,13 +8,14 @@
 </head>
 
 <body>
-    <h1>Products Info</h1>
+    <h1>Products Info
+    </h1>
     <?php
     try {
         $pdo = new PDO("mysql:host=localhost;dbname=my_database", "root", "");
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $stmt = $pdo->query("SELECT * FROM products");
-        echo "<table border='1'>";
+        echo "<table border='1' cellpadding='5'>";
         echo "<tr><th>ID</th><th>Name</th><th>Price</th><th>Category</th></tr>";
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
             echo "<tr>";
