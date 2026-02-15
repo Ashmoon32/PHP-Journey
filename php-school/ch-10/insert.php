6b27558eb814467af164f2e00407328312d9a70c
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-02-15 19:25:38 +0630

    -

---
+++ php-school/ch-10/insert.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Insert Page</title>
+</head>
+
+<body>
+    <form action="" method="POST">
+        Name: <input type="text" name="name" required> <br>
+        Price: <input type="number" name="price" required> <br>
+        Category: <input type="text" name="category" required> <br>
+        <input type="submit" name="submit" value="Add Product">
+    </form>
+</body>
+
+</html>
