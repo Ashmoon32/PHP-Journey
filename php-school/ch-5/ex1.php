e91671d545579ee227ca4f8a43e0823165dd75d0
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-02-18 22:01:18 +0630

    -

--- php-school/ch-5/ex1.php
+++ php-school/ch-5/ex1.php
@@ -9,5 +9,7 @@ $length = 10;
 $width = 5;
 
 echo "Length: $length <br>";
-echo "Width: $width <br>";
+echo "Wdith: $width <br>";
 echo "Area of Rectangle: " . calculateRectangleArea($length, $width);
+
+?>
