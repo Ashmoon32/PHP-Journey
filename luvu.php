3603baca06e99c445da621b0093232727d77add9
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-02-26 16:42:36 +0630

    -

--- luvu.php
+++ luvu.php
@@ -1,6 +1,7 @@
 <?php
-
+echo "<h1> Even Thought I am busy and I can't talk to you, I still luv you KSP!</h1>";
 for ($i = 0; $i < 100; $i++) {
     echo "<p>I Love You In Every Universe KSP!</p>";
+
 }
 ?>
