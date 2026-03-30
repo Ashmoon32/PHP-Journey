1b5ebe67ce416aebd1c24f104ced79c1141e4b79
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-03-30 13:26:08 +0630

    -

---
+++ FPPFE/ch1/ex1.php
@@ -0,0 +1,7 @@
+<?php
+$number = 10;
+$number += 1;
+$number = $number + 1;
+$number++;
+echo "Final Result: $number";
+?>
--- ashashmoon500223/ch-10/ex2.php
+++ ashashmoon500223/ch-10/ex2.php
@@ -8,7 +8,12 @@
 </head>
 
 <body>
-
+    <form method="POST">
+        Name: <input type="text" name="name" required><br>
+        Price: <input type="number" step="0.01" name="price" required><br>
+        Category: <input type="text" name="category" required><br>
+        <input type="submit" name="submit" value="Add Product"><br>
+    </form>
 </body>
 
 </html>
