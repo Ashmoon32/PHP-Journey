fa78d44bc5f6547e9959a2b4a70557e7d10d7c57
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-04-10 23:14:59 +0630

    -

--- test.php
+++ test.php
@@ -4,7 +4,13 @@ include('Library/Helper/Calculator.php');
 
 use Library\Helper\Calculator;
 
-$calc = new Calculator; 
-echo $calc->add([1,2]);
+$calc = new Calculator;
+echo $calc->add([1, 2]);
+echo "\n";
 
-print_r($_SERVER);
+// print_r($_SERVER);
+
+$a = "a";
+$b = [1, 2, 3];
+echo strlen($a) . "\n";
+echo count($b);
